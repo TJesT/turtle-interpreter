137226039083fcd6081358ca392d9e8d5c2e8b16
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2022-04-09 21:27:15 +0700

    Update Direction.java

--- globals/Direction.java
+++ globals/Direction.java
@@ -7,10 +7,10 @@ public class Direction {
 
     private final Map<String, Coords> directions_map =
             new HashMap<String, Coords>() {{
-                put("u", new Coords(0, 1));
-                put("d", new Coords(0, -1));
-                put("r", new Coords(1, 0));
-                put("l", new Coords(-1, 0));
+                put("u", new Coords( 0,  1));
+                put("d", new Coords( 0, -1));
+                put("r", new Coords( 1,  0));
+                put("l", new Coords(-1,  0));
             }};
 
     private boolean isValid(String dir) {
